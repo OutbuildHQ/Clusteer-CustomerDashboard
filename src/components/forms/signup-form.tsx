636b5e1d22c78a8e6578b8734cc058da0621b62e
jason aghedo <jason.aghedo@gmail.com>
2025-06-26 17:49:37 +0100

    feat: added signup form

---
+++ src/components/forms/signup-form.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import { FieldValues, useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import Image from "next/image";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
+const signupFormSchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string(),
+});
+
+type SignupFormType = z.infer<typeof signupFormSchema>;
+
+export default function SignUpForm() {
+	const form = useForm<SignupFormType>({
+		resolver: zodResolver(signupFormSchema),
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: FieldValues) => console.log(values);
+
+	return (
+		<div className="mt-[52px] md:mt-0 p-5 form-border space-y-5 rounded-2xl w-full">
+			<Button
+				variant="outline"
+				asChild
+			>
+				<Link
+					href="/login"
+					className="flex font-mona items-center gap-x-3 py-2.5 w-full h-[44px] google-login-border"
+				>
+					<Image
+						src="/assets/icons/google.svg"
+						alt="google icon"
+						width={24}
+						height={24}
+					/>
+					<span className="text-base text-semibold">Sign up with Google</span>
+				</Link>
+			</Button>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col gap-y-5 w-full"
+				>
+					<FormField
+						control={form.control}
+						name="name"
+						render={({ field }) => (
+							<FormItem className="gap-1.5">
+								<FormLabel className="font-medium">Name</FormLabel>
+								<FormControl>
+									<Input
+										className="focus-visible:ring-0 h-[44px]"
+										placeholder="Enter your name"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem className="gap-1.5">
+								<FormLabel className="font-medium">Email</FormLabel>
+								<FormControl>
+									<Input
+										className="focus-visible:ring-0 h-[44px]"
+										placeholder="Enter your email"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem className="gap-1.5">
+								<FormLabel className="font-medium">Password</FormLabel>
+								<FormControl>
+									<Input
+										className="focus-visible:ring-0 h-[44px]"
+										placeholder="Create a Password"
+										{...field}
+									/>
+								</FormControl>
+								<FormDescription className="text-left text-black text-sm font-lexend">
+									Must be at least 8 characters.
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button
+						type="submit"
+						className="mt-1 font-mona border-black text-[#111111] bg-light-green border font-semibold text-base shadow-xs hover:bg-muted"
+					>
+						Sign up
+					</Button>
+					<div className="text-center">
+						Already have an account?
+						<Link href="/login">
+							<span className="text-dark-green font-semibold ml-1">Log in</span>
+							<ChevronRight className="inline-block size-5 stroke-dark-green ml-2" />
+						</Link>
+					</div>
+				</form>
+			</Form>
+		</div>
+	);
+}
